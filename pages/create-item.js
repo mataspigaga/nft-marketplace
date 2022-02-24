6c7d188c31825ba8c2e0f9452bd98a89a11b3ca8
Matas Pigaga <tech@matas.io>
2022-02-23 22:02:52 -0500

    ignore ignoring...

--- pages/create-item.js
+++ pages/create-item.js
@@ -10,6 +10,7 @@ import { nftaddress, nftmarketaddress } from "../config";
 
 import NFT from "../artifacts/contracts/NFT.sol/NFT.json";
 import Market from "../artifacts/contracts/NFTMarket.sol/NFTMarket.json";
+import Image from "next/image";
 
 export default function CreateItem() {
   const [fileUrl, setFileUrl] = useState(null);
@@ -81,7 +82,7 @@ export default function CreateItem() {
 
   return (
     <div className="flex justify-center">
-      <div className="w-1/2 flex flex-col pb-12">
+      <div className="w-1/2 flex flex-col pb-12 max-w-sm">
         <input
           placeholder="Asset Name"
           className="mt-8 border rounded p-4"
@@ -104,7 +105,17 @@ export default function CreateItem() {
           }
         />
         <input type="file" name="Asset" className="my-4" onChange={onChange} />
-        {fileUrl && <img className="rounded mt-4" width="350" src={fileUrl} />}
+        {fileUrl && (
+          <Image
+            className="rounded mt-4"
+            alt={fileUrl}
+            width="100%"
+            height="100%"
+            layout="responsive"
+            objectFit="contain"
+            src={fileUrl}
+          />
+        )}
         <button
           onClick={createMarket}
           className="font-bold mt-4 bg-pink-500 text-white rounded p-4 shadow-lg"
